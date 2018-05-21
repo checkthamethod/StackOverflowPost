4fd6364c37de0f2aa7fa0dc438f7320021d181f4
Bryan Harris <bryanharris49@gmail.com>
2018-05-21 02:10:25 -0400

    Add project files.

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _date;
+        private int _positiveVoteCount;
+        private int _negativeVoteCount;
+        private string _vote;
+        private bool _isRunning = true;
+
+
+        public void CreatePost()
+        {
+            Console.WriteLine("Title:");
+            _title = Console.ReadLine();
+            Console.WriteLine("Description:");
+            _description = Console.ReadLine();
+            _date = DateTime.Now;
+
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Title: {0}",_title);
+            Console.WriteLine("Description: {0}",_description);
+            Console.WriteLine(_date);
+            Console.WriteLine();
+        }
+
+        public void Vote()
+        {
+            while (_isRunning)
+            {
+                Console.WriteLine("Do you like this post? y/n (To exit press e)");
+                _vote = Console.ReadLine();
+                switch (_vote.ToLower())
+                {
+                    case "y":
+                        UpVote();
+                        break;
+                    case "n":
+                        DownVote();
+                        
+                        break;
+                    case "e":
+                        Console.WriteLine();
+                        Console.WriteLine("Upvotes: {0}, Downvotes: {1}", _positiveVoteCount, _negativeVoteCount);
+                        Console.WriteLine("Exiting application...");
+                        _isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error try one of the inputs above");
+                        break;
+                }
+            }
+        }
+
+        private void UpVote()
+        {
+            _positiveVoteCount++;
+        }
+
+        private void DownVote()
+        {
+            _negativeVoteCount++;
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post();
+            newPost.CreatePost();
+            newPost.ShowPost();
+            newPost.Vote();
+            
+        }
+    }
+}
